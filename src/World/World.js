7a9e3fa81cf6d07bd0e4d952f4476f1b611c338d
naim <naimssj4@gmail.com>
2024-04-11 22:30:30 +0200

    lighting + new material types

--- src/World/World.js
+++ src/World/World.js
@@ -2,6 +2,7 @@ import { createCamera } from "./components/camera.js";
 import { createCube } from "./components/cube";
 import { createScene } from "./components/scene";
 import { createRenderer } from "./systems/renderer";
+import { createLights } from "./components/lights.js";
 import { Resizer } from "./systems/Resizer.js";
 
 class World {
@@ -20,6 +21,9 @@ class World {
     this.#scene.add(cubes.cube2);
     cubes.cube2.position.set(0, 2, 0);
     cubes.cube2.scale.set(0.5, 0.5, 0.5);
+
+    const light = createLights();
+    this.#scene.add(light);
 
     const resizer = new Resizer(container, this.#camera, this.#renderer);
   }
--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -3,6 +3,9 @@ import { WebGLRenderer } from "three";
 function createRenderer() {
   const renderer = new WebGLRenderer();
 
+  // turn on PCL:
+  renderer.physicallyCorrectLights = true;
+
   return renderer;
 }
 
